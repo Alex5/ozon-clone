e62e575f87b7e10ab46d3796f9473f93f26974c2
@a.ilin <a.ilin@invitro.com>
2025-05-20 17:03:06 +0300

    try

--- src/pages/components/product-list/ProductList.tsx
+++ src/pages/components/product-list/ProductList.tsx
@@ -1,15 +1,15 @@
 import { useProducts } from "../../../shared/api/hooks/use-products/useProducts";
-// import { Grid } from "../../../shared/ui/grid/Grid";
+import { Grid } from "../../../shared/ui/grid/Grid";
 import { ProductCard } from "../product-card/ProductCard";
 
 export function ProductList() {
   const { products } = useProducts();
 
   return (
-    <div style={{ display: "flex", flexWrap: "wrap" }}>
+    <Grid>
       {products?.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
-    </div>
+    </Grid>
   );
 }
