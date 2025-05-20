09c3813b9d137d5268a93e70f23d18e0662b071e
@a.ilin <a.ilin@invitro.com>
2025-05-20 16:52:00 +0300

    mocks

--- src/main.tsx
+++ src/main.tsx
@@ -41,7 +41,7 @@ async function enableMocking() {
 
   return worker.start({
     serviceWorker: {
-      url: `${import.meta.env.BASE_URL}mockServiceWorker.js`,
+      url: `${import.meta.env.BASE_URL}/mockServiceWorker.js`,
     },
   });
 }
