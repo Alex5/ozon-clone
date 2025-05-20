7df20b69da234302aae56fdbf0f5cea726ab9d9f
@a.ilin <a.ilin@invitro.com>
2025-05-20 14:53:27 +0300

    fix casing or add missing UI files

--- src/pages/components/product-list/ProductList.tsx
+++ src/pages/components/product-list/ProductList.tsx
@@ -1,6 +1,7 @@
 import { useProducts } from "@shared/api/hooks/use-products";
 import { Grid } from "@shared/ui/grid/Grid";
 import { ProductCard } from "../product-card/ProductCard";
+
 export function ProductList() {
   const { products } = useProducts();
 
