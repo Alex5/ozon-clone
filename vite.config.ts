8906a5fbc5e3bf1f0813a9c704e1e101ede7a0c8
@a.ilin <a.ilin@invitro.com>
2025-05-20 11:34:58 +0300

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig, type UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@shared", replacement: path.resolve(__dirname, "./src/shared") },
+    ],
+  },
 }) satisfies UserConfig;
