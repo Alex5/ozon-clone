6c26b6aa2e2a73030f574afc54c6d2176a430206
@a.ilin <a.ilin@invitro.com>
2025-06-03 00:27:02 +0300

    fix() remove unused package

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 export default defineConfig({
   base: "/react-lavka/",
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    vanillaExtractPlugin({
-      identifiers: ({ hash }) => `reactlavka_${hash}`,
-    }),
-  ],
+  plugins: [react(), tsconfigPaths()],
 });
