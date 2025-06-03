6ced8d558ee08b197b2732ae6ba24910c464e4c9
@a.ilin <a.ilin@invitro.com>
2025-06-03 16:14:20 +0300

    feat() remove motion

--- src/pages/(home)/components/product-card/components/add-to-cart-button/add-to-cart-button.tsx
+++ src/pages/(home)/components/product-card/components/add-to-cart-button/add-to-cart-button.tsx
@@ -3,88 +3,69 @@ import type { ProductType } from "@shared/api/hooks/use-products/types";
 import { Button } from "@shared/ui/button/button";
 import { Text } from "@shared/ui/text/Text";
 import { useCartActions } from "@shared/api/hooks/use-cart/use-cart-actions";
-import { motion, AnimatePresence } from "framer-motion";
+import styles from "./add-to-cart-button.module.css";
 
 export function AddToCartButton({ product }: { product: ProductType }) {
   const { cart } = useCart();
   const { addToCart, removeFromCart } = useCartActions();
   const { quantity } = cart.get(product.id) ?? {};
 
+  const isOpen = !!quantity;
+
   return (
-    <motion.div
-      layout
-      transition={{ type: "tween", ease: "linear", duration: 0.2 }}
-      style={{
-        backgroundColor: "white",
-        borderRadius: "48px",
-        display: "flex",
-        alignItems: "center",
-        gap: "6px",
-      }}
-    >
-      <AnimatePresence initial={false}>
-        {quantity ? (
-          <motion.div
-            key="remove"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ type: "tween", ease: "linear", duration: 0.2 }}
+    <div className={`${styles.container} ${isOpen ? styles.open : ""}`}>
+      <div className={styles["container-inner"]}>
+        <div style={{ display: "flex", height: "100%" }}>
+          <Button
+            onClick={() => removeFromCart(product)}
+            radius="xl"
+            size="icon"
+            shadow="none"
+            colorPallete="gray"
+            style={{
+              height: "inherit",
+              padding: "12px",
+            }}
           >
-            <Button
-              onClick={() => removeFromCart(product)}
-              radius="xl"
-              size="lg"
-              shadow="none"
-              colorPallete="gray"
-              // style={{ padding: "unset" }}
-            >
-              <svg width="24" height="24" viewBox="0 0 24 24">
-                <path
-                  fill="currentColor"
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M3 12.5a.5.5 0 00.5.5h17a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-17a.5.5 0 00-.5.5v1z"
-                />
-              </svg>
-            </Button>
-          </motion.div>
-        ) : null}
-      </AnimatePresence>
-
-      <AnimatePresence initial={false}>
-        {quantity ? (
-          <motion.div
-            key="qty"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ type: "tween", ease: "linear", duration: 0.2 }}
+            <svg width="24" height="24" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M3 12.5a.5.5 0 00.5.5h17a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-17a.5.5 0 00-.5.5v1z"
+              />
+            </svg>
+          </Button>
+          <div
+            style={{
+              height: "100%",
+              paddingBottom: "2px",
+              paddingTop: "2px",
+              width: "100%",
+              display: "block",
+            }}
           >
-            <Text fontWeight="medium">{quantity}</Text>
-          </motion.div>
-        ) : null}
-      </AnimatePresence>
+            <Text fontWeight="medium">{quantity ?? 0}</Text>
+          </div>
 
-      <motion.div layout>
-        <Button
-          onClick={() => addToCart(product)}
-          size="lg"
-          radius="rounded"
-          shadow="none"
-          colorPallete="gray"
-          // style={{ padding: "unset" }}
-        >
-          <svg width="24" height="24" viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M11.5 3a.5.5 0 00-.5.5V11H3.5a.5.5 0 00-.5.5v1a.5.5 0 00.5.5H11v7.5a.5.5 0 00.5.5h1a.5.5 0 00.5-.5V13h7.5a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5H13V3.5a.5.5 0 00-.5-.5h-1z"
-            />
-          </svg>
-        </Button>
-      </motion.div>
-    </motion.div>
+          <Button
+            onClick={() => addToCart(product)}
+            size="icon"
+            radius="rounded"
+            shadow="none"
+            colorPallete="gray"
+            style={{
+              height: "inherit",
+              padding: "12px",
+            }}
+          >
+            <svg width="24" height="24" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M11.5 3a.5.5 0 00-.5.5V11H3.5a.5.5 0 00-.5.5v1a.5.5 0 00.5.5H11v7.5a.5.5 0 00.5.5h1a.5.5 0 00.5-.5V13h7.5a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5H13V3.5a.5.5 0 00-.5-.5h-1z"
+              />
+            </svg>
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
