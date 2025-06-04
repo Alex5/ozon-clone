941012bec47287c86c19e3e549fdf44b27c172f7
@a.ilin <a.ilin@invitro.com>
2025-06-04 20:12:02 +0300

    feat() add cart empty state

--- src/pages/(home)/components/cart-sidebar/components/cart-product/cart-product.tsx
+++ src/pages/(home)/components/cart-sidebar/components/cart-product/cart-product.tsx
@@ -20,10 +20,12 @@ export function CartProduct(props: CartProductProps) {
   const { addToCart, removeFromCart } = useCartActions();
 
   return (
-    <Div flex gap4 itemsCenter>
+    <Div flex gap1 itemsCenter>
       <Image src={snippetImage.url} radius="lg" aspectRatio="1 / 1" size="lg" />
-      <Div flex flexCol gap2 style={{ width: "100%" }}>
-        <Text textStyle="xs">{longTitle}</Text>
+      <Div flex flexCol style={{ width: "100%" }}>
+        <Text textStyle="xs" style={{ marginBottom: "6px" }}>
+          {longTitle}
+        </Text>
         <Text textStyle="sm" fontWeight="medium">
           {currentPriceSigned}
         </Text>
