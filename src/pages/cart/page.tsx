357d5739e8ef915c26a2abad49758ec12b40cff0
Aleksei Ilin <angrymarshall@gmail.com>
2025-06-06 16:45:23 +0300

    Update page.tsx

--- src/pages/cart/page.tsx
+++ src/pages/cart/page.tsx
@@ -1,16 +1,11 @@
 import { useCart } from "@shared/api/hooks/use-cart/use-cart";
 
 export function CartPage() {
-  const { cart } = useCart();
+  const { } = useCart();
 
   return (
     <div>
       Cart:{" "}
-      <ul>
-        {Array.from(cart.values()).map((cartItem) => (
-          <li key={cartItem.product.id}>{cartItem.product.longTitle}</li>
-        ))}
-      </ul>
     </div>
   );
 }
