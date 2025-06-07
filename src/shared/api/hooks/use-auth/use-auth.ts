76ce498bfa5617dbef2f95edea974b97f9f6863c
@a.ilin <a.ilin@invitro.com>
2025-06-07 12:32:46 +0300

    feat() add auth

---
+++ src/shared/api/hooks/use-auth/use-auth.ts
@@ -0,0 +1,47 @@
+import { fetcher } from "@shared/api/fetcher";
+import useSWR from "swr";
+
+export function useAuth() {
+  const { data, mutate } = useSWR<{ username: string }, unknown, string>(
+    "me",
+    (key) => fetcher(key, { credentials: "include" })
+  );
+
+  async function login({ username }: { username: string }) {
+    const res = await mutate(
+      fetcher("login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username,
+        }),
+        credentials: "include",
+      }),
+      {
+        optimisticData: { username },
+        populateCache: true,
+        revalidate: false,
+        rollbackOnError: false,
+      }
+    );
+
+    return res;
+  }
+
+  async function logout() {
+    await mutate(undefined, { revalidate: false });
+
+    await fetcher("logout", {
+      method: "PATCH",
+      credentials: "include",
+    });
+
+    await mutate();
+  }
+
+  return {
+    me: data,
+    login,
+    logout,
+  };
+}
