f0467187ff360257ff87186050d80527c044a4fd
@a.ilin <a.ilin@invitro.com>
2025-06-10 20:53:59 +0300

    feat() add vite-plugin-cache

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePluginCache } from "vite-plugin-cache";
 
 export default defineConfig({
   base: "/react-lavka/",
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), vitePluginCache()],
   server: {
     proxy: {
       "/api": {
