75a8c2185ea5e6458f3150841aa9c6833f2f9a32
@a.ilin <a.ilin@invitro.com>
2025-06-10 23:30:15 +0300

    feat() update plug

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { vitePluginCache } from "vite-plugin-cache";
 
-const plugins: PluginOption[] = [react(), tsconfigPaths(), vitePluginCache()];
+const plugins: PluginOption[] = [
+  react(),
+  tsconfigPaths(),
+  vitePluginCache({
+    apiUrlPatter: /^https:\/\/alex5-hono-mock-api-9e61\.twc1\.net\/api\//,
+  }),
+];
 
 export default defineConfig({
   base: "/react-lavka/",
