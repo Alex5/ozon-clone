fb7e4c899324e8163e2a5a0868df05b21fc0cb6b
@a.ilin <a.ilin@invitro.com>
2025-06-15 13:28:33 +0300

    feat() update vite-cache-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,32 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { vitePluginCache } from "vite-plugin-cache";
 
-const plugins: PluginOption[] = [react(), tsconfigPaths(), vitePluginCache()];
+const plugins: PluginOption[] = [
+  react(),
+  tsconfigPaths(),
+  vitePluginCache({
+    config: (dc) => ({
+      ...dc,
+      "swr-api-cache": {
+        ...dc["swr-api-cache"],
+        match: (args) =>
+          dc["swr-api-cache"].match(args) &&
+          !args.url.pathname.includes("/api/v1/cart"),
+      },
+      "newtwork-first-api-cache": {
+        match: ({ url, request }) =>
+          url.pathname.includes("/api/v1/cart") && request.method === "GET",
+        strategy: "network-first",
+        plugins: {
+          expiration: {
+            maxAgeSeconds: 60 * 5,
+            maxEntries: 100,
+          },
+        },
+      },
+    }),
+  }),
+];
 
 export default defineConfig({
   base: "/react-lavka/",
