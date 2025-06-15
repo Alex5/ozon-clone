444d8c204257e5b41ff9565f682e7147e38f1997
@a.ilin <a.ilin@invitro.com>
2025-06-15 20:25:03 +0300

    fix()

--- src/pages/(home)/components/product-list/product-list.tsx
+++ src/pages/(home)/components/product-list/product-list.tsx
@@ -1,9 +1,12 @@
 import { useProducts } from "@shared/api/hooks/use-products/useProducts";
 import { Grid } from "@shared/ui/grid/grid";
 import { ProductCard } from "../product-card/product-card";
+import { ProductListSekeltom } from "./product-list.skeleton";
 
 export function ProductList() {
-  const { products } = useProducts();
+  const { products, isLoading } = useProducts();
+
+  if (isLoading) return <ProductListSekeltom />;
 
   return (
     <Grid>
